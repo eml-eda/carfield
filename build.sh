22c60bed03a05724404bcc765af1c555c63d9464
Lorenzo Ruotolo <ruotololorenzo@gmail.com>
2026-01-13 19:19:22 +0100

    Fix to scripts

--- build.sh
+++ build.sh
@@ -5,9 +5,9 @@
 #########################################################
 
 # Source environment
-source ./env/env-non-iis.sh #pulp sdk already there
-source ./pulp_cluster/pulp-runtime/configs/carfield-cluster.sh && \
+source ./pulp_cluster/pulp-runtime/configs/carfield-cluster.sh
 
 # Build the application, overriding CHS_SW_FLAGS to ignore some warnings
-export CHS_SW_FLAGS="-DOT_PLATFORM_RV32 -march=rv64gc -mabi=lp64d -mstrict-align -O2 -Wall -Wno-implicit-function-declaration -Wno-int-conversion -Wno-incompatible-pointer-types -Wextra -static -ffunction-sections -fdata-sections -frandom-seed=cheshire -fuse-linker-plugin -flto -Wl,-flto" && \
-make -j pulpd-sw-build -B -o pulpd-sw-init car-pulpd-sw-offload-tests
+export CHS_SW_FLAGS="-DOT_PLATFORM_RV32 -march=rv64gc -mabi=lp64d -mstrict-align -O2 -Wall -Wno-implicit-function-declaration -Wno-int-conversion -Wno-incompatible-pointer-types -Wextra -static -ffunction-sections -fdata-sections -frandom-seed=cheshire -fuse-linker-plugin -flto -Wl,-flto"
+make -j pulpd-sw-build -o pulpd-sw-init 
+make -j car-pulpd-sw-offload-tests
--- setup.sh
+++ setup.sh
@@ -4,23 +4,19 @@
 # This script is used to setup the environment for carfield #
 #############################################################
 
+git submodule update --init --recursive
+
 # Build and activate the venv
-make venv && \
-source ./.venv/bin/activate && \
+make venv
+source ./.venv/bin/activate
 
 # Build needed components once
-git submodule update --init --recursive && \
-make car-update-deps && \
+cd ./cheshire
+make -j clint
+make -j chs-sw-all 
+cd ../
+make -j car-sw-libs -B
+make -j pulpd-sw-init
 
-cd ./cheshire && \
-make -j clint chs-sw-all && \
-cd ../ && \
-make -j car-sw-libs -B && \
-
-cd ./pulp_cluster/ && \
-make pulp-runtime && \
-cd ../ && \
-
-source ./pulp_cluster/pulp-runtime/configs/carfield-cluster.sh && \
-make -j pulpd-sw-init -B && \
+source ./env/env-non-iis.sh
 echo "Environment setup completed."
